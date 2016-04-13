466ffc3b6bea6d726b375cd7bc747830dc139801
Patrick Bell <widoreu.bell@gmail.com>
2016-04-13 08:19:20 -0500

    Initial commit! Well, not really.

---
+++ CTF/Controller/ControllerMain.cs
@@ -0,0 +1,28 @@
+﻿using RobloxSharp;
+using SaneWeb.Resources.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CTF.Controller
+{
+    public static class ControllerMain
+    {
+        [Controller("~/a/")]
+        public static String a(HttpListenerContext context, String body, String id)
+        {
+            String actual = Encoding.ASCII.GetString(Convert.FromBase64String(id));
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://www.roblox.com/Login/Negotiate.ashx?suggest=" + actual);
+            CookieContainer container = new CookieContainer();
+            request.Headers.Add("RBXAuthenticationNegotiation", @": http://www.roblox.com");
+            request.Headers.Set(HttpRequestHeader.AcceptEncoding, "gzip");
+            request.Headers.Set(HttpRequestHeader.AcceptLanguage, "en-US,*");
+            request.CookieContainer = container;
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            return RobloxUtils.buildCookieString(container, response);
+        }
+    }
+}
---
+++ CTF/Program.cs
@@ -0,0 +1,26 @@
+﻿using SaneWeb.Resources;
+using SaneWeb.Web;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CTF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SaneServer ws = new SaneServer(
+                (Utility.fetchFromResource(true, Assembly.GetExecutingAssembly(), "CTF.Resources.ViewStructure.xml")),
+                "Database\\SaneDB.db", true,
+                "http://+:80/");
+            ws.addController(typeof(Controller.ControllerMain));
+            ws.run();
+            Console.WriteLine("Running!");
+            Console.ReadKey();
+        }
+    }
+}
